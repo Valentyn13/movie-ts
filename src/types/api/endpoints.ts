2174f0a9a9fa41c56c4c6a339af0fd7fd5e1237d
Valentyn13 <valentyn13git@gmail.com>
2024-01-16 16:07:41 +0200

    movie-ts: + ApiEndpoints enum - create constants for api endpoints

---
+++ src/types/api/endpoints.ts
@@ -0,0 +1,7 @@
+export enum ApiEndpoints {
+    PopularMovies = 'https://api.themoviedb.org/3/movie/popular?language=en-US',
+    UpcommingMovies = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US',
+    TopRatedMovies = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US',
+    SearchByNameMovies = 'https://api.themoviedb.org/3/search/movie?&include_adult=false&language=en-US',
+    MovieDetails = 'https://api.themoviedb.org/3/movie/',
+}
