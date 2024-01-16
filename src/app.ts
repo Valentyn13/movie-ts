37af1c4b50568b02b35a6e1373e7709c9cf7a195
Valentyn13 <valentyn13git@gmail.com>
2024-01-16 16:39:35 +0200

    movie-ts: + App component and connect it to main.ts

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import Header from './components/header/header';
+import Main from './components/main/mainComponent';
+import { popularMoviesApi } from './services/moviesApi';
+import { IMovieList } from './types/response/mapperResponse';
+
+const header = new Header();
+
+export interface IAppData {
+    currentPage: number;
+    movies: IMovieList;
+}
+
+class App {
+    static rootElement = document.getElementById('root');
+
+    static loadingElement = document.getElementById('loading-overlay');
+
+    static appData: IAppData;
+
+    static async startApp() {
+        const initMoviesData = await popularMoviesApi.getMovies();
+
+        App.appData = {
+            movies: initMoviesData.movies,
+            currentPage: initMoviesData.page,
+        };
+
+        const { headerComponent, favouriteComponent } = await header.render();
+
+        const mainComponent = Main.render(App.appData);
+
+        App.rootElement?.appendChild(headerComponent);
+        App.rootElement?.appendChild(favouriteComponent);
+        App.rootElement?.appendChild(mainComponent);
+    }
+}
+
+export default App;
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './styles/styles.css';
+import App from './app';
 
 // TODO render your app here
+
+App.startApp();
