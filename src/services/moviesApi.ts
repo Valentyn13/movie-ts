b0260002067f2e800cf0667cf2b9cb916c5a88cc
Valentyn13 <valentyn13git@gmail.com>
2024-01-16 16:19:42 +0200

    movie-ts: + MoviesApi - serviece that make request and normalize data

---
+++ src/services/moviesApi.ts
@@ -0,0 +1,34 @@
+import { moviesMapper } from '../helpers/mapper';
+import { ApiEndpoints } from '../types/api/endpoints';
+
+class MoviesApi {
+    #apiPath: ApiEndpoints;
+
+    constructor(apiPath: ApiEndpoints) {
+        this.#apiPath = apiPath;
+    }
+
+    async getMovies(page?: number, name?: string) {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization:
+                    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjQyYzcxOWIyNjA2YTg2MDNlYjg5MDVmZmM3NGNlZSIsInN1YiI6IjY1YTNhNDQ5YmMyY2IzMDBiZTAyMjc2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tWRGYzerYDH772JBhndICWbOtuG1TRdzPRBo6ZL1yoo',
+            },
+        };
+        const configurePage = page ? `${this.#apiPath}&page=${page}` : `${this.#apiPath}&page=1`;
+        const configurePageAndName = name ? `${configurePage}&query=${name}` : configurePage;
+
+        const response = await fetch(configurePageAndName, options);
+        const data = await response.json();
+
+        const mapperedMovies = moviesMapper(data);
+        return mapperedMovies;
+    }
+}
+
+export const popularMoviesApi = new MoviesApi(ApiEndpoints.PopularMovies);
+export const upcumingMoviesApi = new MoviesApi(ApiEndpoints.UpcommingMovies);
+export const topRatedMoviesApi = new MoviesApi(ApiEndpoints.TopRatedMovies);
+export const searchByNameMovies = new MoviesApi(ApiEndpoints.SearchByNameMovies);
