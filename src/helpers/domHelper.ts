1006df6b09607d8bfd8621da12628d82342c5f53
Valentyn13 <valentyn13git@gmail.com>
2024-01-16 16:18:02 +0200

    movie-ts: + domHelpers.ts file - file with dom helpers

---
+++ src/helpers/domHelper.ts
@@ -0,0 +1,26 @@
+interface ICreateElementParams {
+    tagName: string;
+    className: string;
+    attributes: {
+        [key: string]: string;
+    };
+}
+
+export default function createElement({ tagName, className, attributes = {} }: ICreateElementParams): HTMLElement {
+    const element = document.createElement(tagName);
+
+    if (className) {
+        const classNames = className.split(' ').filter(Boolean);
+        element.classList.add(...classNames);
+    }
+
+    Object.keys(attributes).forEach((key) => element.setAttribute(key, attributes[key]));
+
+    return element;
+}
+
+export const removeAllChildNodes = (parent: HTMLElement) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};
