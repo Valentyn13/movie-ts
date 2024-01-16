a58b2a2fb9e57d6a62b0b5171485708981685542
Valentyn13 <valentyn13git@gmail.com>
2024-01-16 16:35:14 +0200

    movie-ts: + Album component

---
+++ src/components/album/album.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-plusplus */
+import createElement from '../../helpers/domHelper';
+import { IMovieList } from '../../types/response/mapperResponse';
+import FilmCard from '../filmCard/filmCard';
+import LoadMore from '../loadMore/loadMore';
+
+class Album {
+    static createContent(movieList: IMovieList) {
+        const filmConatainer = createElement({
+            tagName: 'div',
+            className: 'row',
+            attributes: { id: 'film-container' },
+        });
+
+        for (let i = 0; i < movieList.length; i++) {
+            const card = FilmCard.render(movieList[i]);
+            filmConatainer.appendChild(card);
+        }
+        return filmConatainer;
+    }
+
+    static render(movieList: IMovieList) {
+        const albumComponent = createElement({ tagName: 'div', className: 'album py-5 bg-light', attributes: {} });
+
+        const loadMore = LoadMore.render();
+
+        const container = createElement({
+            tagName: 'div',
+            className: 'container',
+            attributes: { id: 'film-container-wrapper' },
+        });
+
+        const filmConatainer = Album.createContent(movieList);
+
+        container.appendChild(filmConatainer);
+
+        albumComponent.appendChild(container);
+        albumComponent.appendChild(loadMore);
+
+        return albumComponent;
+    }
+}
+
+export default Album;
